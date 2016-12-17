6e7c634d4be804dbb82abc7ef5bcf837aa9d540b
Ricky Dam <ricky.dam10@hotmail.com>
2016-12-16 20:11:09 -0500

    Connected the app to an mLab database

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
 
+// mongoose setup
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://heroku_gm3w4xqc:j6lml1quargpb15hqv2lmj2eb5@ds133328.mlab.com:33328/heroku_gm3w4xqc');
+mongoose.connection.once('connected', function() {
+  console.log("Connected successfully to mLab database: heroku_gm3w4xqc");
+});
+
 // morgan setup
 var morgan = require('morgan'); // log request details
 app.use(morgan('dev'));
