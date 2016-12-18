060b1c75900cb92fb0601363fe0e4423984727d4
Ricky Dam <ricky.dam10@hotmail.com>
2016-12-17 19:48:37 -0500

    Initialized the default state properly

--- controllers/stateController.js
+++ controllers/stateController.js
@@ -1,5 +1,17 @@
+// Create an app variable containing the ui-router module
 var app = angular.module('theApp', ['ui.router']);
 
+// Initialize the state to "home" on first visit
+app.run(['$state', function($state) {
+  $state.transitionTo('home');
+}]);
+
+// Constantly check to see if the state changed
+app.controller('stateController', function($scope, $state) {
+  $scope.$state = $state;
+});
+
+// Route depending on the provided state
 app.config(function($stateProvider) {
   $stateProvider
     .state('home', {
@@ -14,12 +26,4 @@ app.config(function($stateProvider) {
       url: '/login',
       templateUrl: 'views/login.html'
     })
-    .state("otherwise", {
-      url: '*path',
-      templateUrl: "views/home.html"
-    });
-});
-
-app.controller('stateController', function($scope, $state) {
-  $scope.$state = $state;
 });
